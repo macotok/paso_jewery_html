08f1d9d6f8f48fd0aa0204e80dd6d6a7f5e9db4a
macotok <monochromer@r8.ucom.ne.jp>
2018-11-26 22:57:42 +0900

    make development

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = [
+  {
+    entry: {
+      app: [
+        './src/js/app.js',
+        './src/sass/style.scss',
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      publicPath: '/',
+    },
+    devServer: {
+      contentBase: './dist/html/',
+      watchContentBase: true,
+      port: 3000,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: MiniCssExtractPlugin.loader,
+          }, {
+            loader: 'css-loader',
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')({
+                  browsers: ['last 2 versions', 'Android >= 4'],
+                }),
+              ],
+            },
+          }, {
+            loader: 'sass-loader',
+          }],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'src/img/'),
+          to: path.resolve(__dirname, 'dist/img/'),
+        },
+      ]),
+      new MiniCssExtractPlugin({
+        filename: 'css/style.css',
+      }),
+      new ImageminPlugin({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        pngquant: {
+          quality: '95-100',
+        },
+      }),
+    ],
+  },
+];
