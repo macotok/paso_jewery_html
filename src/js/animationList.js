107fea561859ec095b89a3dafb4cdabc1fde51e2
macotok <monochromer@r8.ucom.ne.jp>
2018-12-24 22:26:18 +0900

    finish script collectionList animation

--- src/js/animationList.js
+++ src/js/animationList.js
@@ -2,16 +2,27 @@ export default class AnimationList {
   constructor(hasTarget, targetDom) {
     this.hasTarget = document.getElementById(hasTarget);
     this.targetDom = document.querySelectorAll(targetDom);
+    this.targetDomPositionList = [];
     this.scrollEventFlag = true;
     this.observeTime = 200;
+    this.controlPosition = 100;
+    this.observeResize = false;
+    this.fadeInUp = 'fadeInUp';
+    this.styleVisible = 'visible';
   }
 
   positionDataSet() {
     this.targetDom.forEach((t) => {
-      const domList = t;
-      domList.dataset.position = domList.offsetTop;
+      this.targetDomPositionList.push(t.offsetTop);
     });
+    this.getScreenTop();
+    this.resizeWindow();
+  }
+
+  getScreenTop() {
     this.scrollEvent();
+    const scrollTopPosition = document.scrollingElement.scrollTop;
+    this.checkIsAnimation(scrollTopPosition);
   }
 
   scrollEvent() {
@@ -19,10 +30,32 @@ export default class AnimationList {
       if (this.scrollEventFlag) {
         this.scrollEventFlag = false;
         setTimeout(() => {
-          const scrollTop = document.scrollingElement.scrollTop;
+          const scrollTopPosition = document.scrollingElement.scrollTop;
+          this.checkIsAnimation(scrollTopPosition);
           this.scrollEventFlag = true;
         }, this.observeTime);
       }
+    }, false);
+  }
+
+  checkIsAnimation(scrollTopPosition) {
+    const windowHeight = document.documentElement.clientHeight;
+    this.targetDomPositionList.filter((t, index) => {
+      if (t <= scrollTopPosition + windowHeight - this.controlPosition) {
+        this.targetDom[index].style.visibility = this.styleVisible;
+        this.targetDom[index].classList.add(this.fadeInUp);
+      }
+      return false;
+    });
+  }
+
+  resizeWindow() {
+    window.addEventListener('resize', () => {
+      clearTimeout(this.observeResize);
+      this.observeResize = setTimeout(() => {
+        this.targetDomPositionList = [];
+        this.positionDataSet();
+      }, this.observeTime);
     }, false);
   }
 }
