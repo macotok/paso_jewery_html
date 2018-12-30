a6ab438a511a1d8df916fc62f375170359e0bc9b
macotok <monochromer@r8.ucom.ne.jp>
2018-12-30 16:20:30 +0900

    fix navlist

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import AnimationList from './animationList';
 window.onload = () => {
   // スマホドロワー
   const menuSp = new DrawerSp('.m-nav-02-icon', '.m-nav-04', '.m-cancel-01-icon', 0);
-  const calendarSp = new DrawerSp('.m-nav-03-icon', '.m-nav-05', '.m-cancel-01-icon', 1);
+  const calendarSp = new DrawerSp('.m-nav-03-icon', '.m-calendar-02', '.m-cancel-01-icon', 1);
   if (menuSp.eventTag && calendarSp.eventTag) {
     menuSp.event();
     calendarSp.event();
